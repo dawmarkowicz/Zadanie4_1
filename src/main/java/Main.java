b72acc4459b62d5d41dc169a87bd65804058d312
dawmarkowicz <daw.markowicz123@gmail.com>
2025-10-27 08:57:08 +0000

    zadanie2_6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,25 @@
-
+import java.util.Scanner;
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 // import org.junit.jupiter.api.Test;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            if (i % 2 == 0)
-                System.out.println(i);
-        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe:");
+        int liczba = scanner.nextInt();
+        boolean czyPierwsza = true;
+
+        for (int i = 2; i < liczba; i++)
+            if (liczba % i == 0) {
+                czyPierwsza = false;
+            }
+        if (czyPierwsza)
+            System.out.println("Liczba jest pierwsza");
+        else
+            System.out.println("Liczba nie jest pierwsza");
+        scanner.close();
     }
 }
-
 // @Test
 // void addition() {
 // assertEquals(2, 1 + 1);
