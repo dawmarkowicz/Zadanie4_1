072931f31823cf700bcdfdfcdbd31b581f733bbc
dawmarkowicz <daw.markowicz123@gmail.com>
2025-11-24 08:10:31 +0000

    zadanie3_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,24 @@
 import java.util.Scanner;
+
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 // import org.junit.jupiter.api.Test;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj liczbe:");
-        int liczba = scanner.nextInt();
-        boolean czyPierwsza = true;
+        System.out.println("Podaj rozmiar: ");
+        int rozmiar = scanner.nextInt();
 
-        for (int i = 2; i < liczba; i++)
-            if (liczba % i == 0) {
-                czyPierwsza = false;
+        for (int i = 0; i <= rozmiar; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
             }
-        if (czyPierwsza)
-            System.out.println("Liczba jest pierwsza");
-        else
-            System.out.println("Liczba nie jest pierwsza");
+            System.out.println();
+        }
         scanner.close();
     }
 }
+
 // @Test
 // void addition() {
 // assertEquals(2, 1 + 1);
