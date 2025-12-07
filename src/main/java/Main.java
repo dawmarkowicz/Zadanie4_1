91bd6daa9ab852e6d19acbdd0f24f8e9911c9156
dawmarkowicz <daw.markowicz123@gmail.com>
2025-12-07 11:03:07 +0000

    zadanie3_4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            for (int j = 1; j <= 10; j++) {
-                int wynik = i * j;
-                System.out.printf("%4d", wynik);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj liczbe: ");
+        int n = scanner.nextInt();
+        if (n < 0) {
+            System.out.println("Liczba nie moze byc ujemna");
+        } else {
+            long silnia = 1;
+            for (int i = 1; i <= n; i++) {
+                silnia *= i;
             }
-            System.out.println();
+            System.out.println("Silnia liczby " + n + " wynosi: " + silnia);
         }
     }
 }
