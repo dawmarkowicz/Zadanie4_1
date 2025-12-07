753d2b6765246c4935cfc67b343c178366fc6ff0
dawmarkowicz <daw.markowicz123@gmail.com>
2025-12-07 10:56:41 +0000

    zadanie3_4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,13 @@
 //import java.util.Scanner;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import org.junit.jupiter.api.Test;
-
 public class Main {
     public static void main(String[] args) {
-        int n = 5;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 || i == n - 1 || j == 0 || j == n - 1)
-                    System.out.print("*");
-                else
-                    System.out.print(" ");
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                int wynik = i * j;
+                System.out.printf("%4d", wynik);
             }
             System.out.println();
         }
     }
 }
-
-// @Test
-// void addition() {
-// assertEquals(2, 1 + 1);
-// }
