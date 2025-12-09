28b34028d89f611c97ae90a116e7efb3d14861a9
dawmarkowicz <daw.markowicz123@gmail.com>
2025-12-09 17:50:54 +0000

    zadanie4_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,18 +4,26 @@ public class Main {
     public static void main(String[] args) {
         Service service = new Service();
 
-        // dodawanie studenta
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj imie studenta: ");
-        String imie = scanner.nextLine();
-        System.out.println("Podaj wiek studenta: ");
-        int wiek = scanner.nextInt();
+        System.out.println("1 - Dodaj nowego studenta: ");
+        System.out.println("2 - Wypisz wszystkich studentow:");
+        int wybor = scanner.nextInt();
+        scanner.nextLine();
 
-        Student student = new Student(imie, wiek);
+        if (wybor == 1) {
+            System.out.println("Podaj imie studenta: ");
+            String imie = scanner.nextLine();
+            System.out.println("Podaj wiek studenta: ");
+            int wiek = scanner.nextInt();
+            Student student = new Student(imie, wiek);
+            service.zapiszStudenta(student);
 
-        service.zapiszStudenta(student);
-        service.wypiszStudentow();
-
+            System.out.println("Dodano studenta");
+        } else if (wybor == 2) {
+            service.wypiszStudentow();
+        } else {
+            System.out.println("Niepoprawny wybor");
+        }
         scanner.close();
     }
 }
